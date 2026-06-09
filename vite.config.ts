41457de44a029965e94f48ef60e0870b9acdd0fe
lhdfts <di03@tradestars.com.br>
2026-06-08 21:44:39 -0300

    chore: fix VULN-002 by using terser and dropping console/comments

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
 })
