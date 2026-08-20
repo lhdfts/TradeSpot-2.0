d8aafd9bf67aa8e37226c1b40aa257fa1af495aa
lhdfts <di03@tradestars.com.br>
2026-08-20 10:27:00 -0300

    fix: remove accidentally compiled javascript files

--- src/services/api.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = void 0;
-const supabaseApi_1 = require("./supabaseApi");
-exports.api = new supabaseApi_1.SupabaseApiService();
--- src/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APPOINTMENT_STATUSES = void 0;
-exports.APPOINTMENT_STATUSES = [
-    'Cancelado', 'Esquecimento', 'No-show', 'Pendente', 'Realizado', 'Reagendado'
-];
